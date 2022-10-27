36ef6097e4c0f8ea0f53eb94545ce897ffbcf506
vijaybhaskar <vijaybhaskar489@gmail.com>
2022-10-27 08:24:01 +0400

    initial commit

---
+++ docker-compose/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker compose exec app composer config --no-plugins allow-plugins.pestphp/pest-plugin true
+docker-compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
+
+docker-compose exec app php artisan storage:link || true
+docker-compose exec app php artisan key:generate
